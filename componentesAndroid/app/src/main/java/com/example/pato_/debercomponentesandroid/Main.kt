10af11ba9988430b536fd7ce4f0ef58465f903b7
Richard <rchrdbrneo@gmail.com>
2018-11-15 14:00:33 -0500

    ComponentesDeber

---
+++ componentesAndroid/app/src/main/java/com/example/pato_/debercomponentesandroid/Main.kt
@@ -0,0 +1,12 @@
+package com.example.pato_.debercomponentesandroid
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Main : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
